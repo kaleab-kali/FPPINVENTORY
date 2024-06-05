4f4c147a4b247a15b71049ca2027af02d4eeeb95
Mulualem Mekonnen <mulualemasfaw2321@gmail.com>
2024-06-05 22:38:22 +0300

    add fetcher for unique items using employee id

--- server/controllers/uniqueItemController.ts
+++ server/controllers/uniqueItemController.ts
@@ -12,4 +12,21 @@ const getAllUniqueItems = async (req: Request, res: Response): Promise<void> => 
     }
   };
 
-export {getAllUniqueItems };
+  const getUniqueItemsByEmployeeId = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { employeeId } = req.params;
+      const uniqueItems = await UniqueItem.find({ employeeId });
+  
+      if (!uniqueItems) {
+        res.status(404).json({ message: "No unique items found for the specified employee ID." });
+        return;
+      }
+  
+      res.status(200).json(uniqueItems);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+
+export {getAllUniqueItems, getUniqueItemsByEmployeeId };
--- server/routes/uniqueItemRoutes.ts
+++ server/routes/uniqueItemRoutes.ts
@@ -1,10 +1,13 @@
 import express, { Router } from "express";
-import { getAllUniqueItems } from "../controllers/uniqueItemController";
+import { getAllUniqueItems, getUniqueItemsByEmployeeId } from "../controllers/uniqueItemController";
 import authAdminProtect from '../middleware/authAdminMiddleware';
 
 const router: Router = express.Router();
 
 // Get all unique items
 router.get("/", authAdminProtect, getAllUniqueItems);
+
+//Route to get unique items by employee ID
+router.get("/employee/:employeeId", getUniqueItemsByEmployeeId);
 
 export default router;
